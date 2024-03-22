86e62471601fffb5a83a9e3e290db7c8221da572
ScarLLD <www.olegborisov1@gmail.com>
2024-03-22 16:47:29 +0300

    Counted

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private float _time;
+    [SerializeField] private Button _actionButton;
+
+    private Coroutine _countCoroutine;
+    private WaitForSeconds _wait;
+    private bool _isCount = false;
+    private int _counter;
+
+    public event Action<int> CountChanged;
+
+    private void Awake()
+    {
+        _wait = new WaitForSeconds(_time);
+    }
+
+    private void OnEnable()
+    {
+        _actionButton.onClick.AddListener(ChangeCountBool);
+    }
+    private void OnDisable()
+    {
+        _actionButton.onClick.RemoveListener(ChangeCountBool);
+    }
+
+    private void ChangeCountBool()
+    {
+        _isCount = !_isCount;
+
+        if (_isCount && _countCoroutine == null)
+            _countCoroutine = StartCoroutine(Count());
+    }
+
+    private IEnumerator Count()
+    {
+        while (_isCount)
+        {
+            _counter++;
+
+            CountChanged?.Invoke(_counter);
+
+            yield return _wait;
+        }
+
+        _countCoroutine = null;
+    }
+}
---
+++ Assets/Scripts/ScoreView.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _CountText;
+    [SerializeField] private Counter _counter;
+
+    private void OnEnable()
+    {
+        _counter.CountChanged += OnCountChanged;
+    }
+
+    private void OnDisable()
+    {
+        _counter.CountChanged -= OnCountChanged;
+    }
+
+    private void OnCountChanged(int count)
+    {
+        _CountText.text = count.ToString();
+    }
+}
